1dabeda8d88f3e8ddbb326e5e23118feb39e4581
仲 仲 <2544054211@qq.com>
2020-07-07 17:45:43 +0800

    还原库

--- Project1/Project1/run.cpp
+++ Project1/Project1/run.cpp
@@ -4,14 +4,3 @@
 	vs & git
 */
 
-#include <iostream>
-
-int main()
-{
-	std::cout << "hello, world." << std::endl;
-	
-	/// dev branch
-	std::cout << "hello, dev branch." << std::endl;
-
-	return 0;
-}
